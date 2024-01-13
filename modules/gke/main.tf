dcd85ceaa2f3abf6494314a593a33de0c202c3f6
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:31:11 +0530

    Delete modules directory

--- modules/gke/main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_container_cluster" "cluster" {
-  name     = var.cluster_name
-  location = var.region
-
-  network    = google_compute_network.vpc.name
-  subnetwork = var.subnet_id
-
-  # Configure node pools, IP masquerading, etc. as needed
-}
