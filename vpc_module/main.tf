945dbbff0b8f6d417ef7f7bc784e4f85632cfa09
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 10:09:26 +0530

    Update main.tf

--- vpc_module/main.tf
+++ vpc_module/main.tf
@@ -9,10 +9,8 @@ module "vpc" {
 
   project_id      = var.project_id
   network_name    = var.vpc_name
-  subnetwork_name = var.subnet_name
+  subnets         = [var.subnet_name]
+  location        = var.region
   ip_cidr_range   = "10.0.0.0/16"
-  region          = var.region
-  subnetwork_secondary_ip_range = {
-    var.subnet_name = [var.ip_range_pods]
-  }
+  subnetwork_secondary_ip_range = {}
 }
