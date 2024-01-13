e82555939ec0fdb63da6df3a3f8a04c03798fe64
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:34:27 +0530

    Update gke_module.tf

--- gke_module.tf
+++ gke_module.tf
@@ -7,7 +7,7 @@ module "gke" {
   network          = module.vpc.network_name
   subnetwork       = module.vpc.subnetwork_name
   node_pool_name   = var.node_pool_name
-  node_count       = 1
+  node_count       = 3
   machine_type     = var.machine_type
   node_locations   = [var.region]
   enable_autoscaling = false
