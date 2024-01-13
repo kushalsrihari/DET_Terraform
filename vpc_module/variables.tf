50d35f49feb20ea37c1fcafc6b322e9699b0e5ef
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 10:18:55 +0530

    Create variables.tf

---
+++ vpc_module/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
