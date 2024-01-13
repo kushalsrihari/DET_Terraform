ae4c96d9e5cf53d512a7072444aee56e54e5a3e0
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:32:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,13 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.49.0"
-    }
-  }
-}
-
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  credentials = file("awesome-gist-402911-68434aac7771.json")
+  project     = var.project_id
+  region      = var.region
 }
 
 module "vpc" {
-  source = "./modules/vpc"
-
-  project_id   = var.project_id
-  region       = var.region
-  vpc_name     = "my-vpc"
-  subnet_name  = "my-subnet"
-  cidr_block   = "10.0.0.0/16"
+  source    = "./vpc_module"
 }
 
 module "gke" {
-  source = "./modules/gke"
-
-  project_id   = var.project_id
-  region       = var.region
-  cluster_name = "my-gke-cluster"
-  subnet_id    = module.vpc.subnet_id
+  source          = "./gke_module"
 }
