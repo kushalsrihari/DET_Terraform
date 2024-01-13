ad4aa1099cd2454586ca28aded5534c463a5019e
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:42:50 +0530

    Update gke_module.tf

--- gke_module.tf
+++ gke_module.tf
@@ -1,14 +1,13 @@
 module "gke" {
-  source = "terraform-google-modules/kubernetes-engine/google"
-
-  cluster_name     = var.cluster_name
-  project_id       = var.project_id
-  region           = var.region
-  network          = module.vpc.network_name
-  subnetwork       = module.vpc.subnetwork_name
-  node_pool_name   = var.node_pool_name
-  node_count       = 3
-  machine_type     = var.machine_type
-  node_locations   = [var.region]
+  source          = "terraform-google-modules/kubernetes-engine/google"
+  cluster_name    = var.cluster_name
+  project_id      = var.project_id
+  region          = var.region
+  network         = module.vpc.network_name
+  subnetwork      = module.vpc.subnetwork_name
+  node_pool_name  = var.node_pool_name
+  node_count      = 3
+  machine_type    = var.machine_type
+  node_locations  = [var.region]
   enable_autoscaling = false
 }
