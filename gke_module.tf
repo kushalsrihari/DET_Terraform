9b6517aa45cd1bfdaab9a6df0931f7f507561c9c
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:31:58 +0530

    Update gke_module.tf

--- gke_module.tf
+++ gke_module.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  credentials = file("awesome-gist-402911-68434aac7771.json")
-  project     = var.project_id
-  region      = var.region
-}
-
 module "gke" {
   source = "terraform-google-modules/kubernetes-engine/google"
 
