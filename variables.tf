d76360125a834a2992434b5e532bfd71146f3eac
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:28:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,7 @@
 variable "project_id" {
-  description = "GCP project ID"
+  type = string
 }
 
 variable "region" {
-  description = "GCP region"
-}
-
-variable "cluster_name" {
-  description = "Name of the GKE cluster"
-  default     = "my-gke-cluster"
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC"
-  default     = "my-vpc"
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  default     = "my-subnet"
-}
-
-variable "node_pool_name" {
-  description = "Name of the node pool"
-  default     = "default-pool"
-}
-
-variable "machine_type" {
-  description = "Machine type for GKE nodes"
-  default     = "e2-medium"
+  type = string
 }
