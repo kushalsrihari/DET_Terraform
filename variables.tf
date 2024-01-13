ccbeab1e7f6222bfb01123c3d40b465867eba696
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:55:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,20 +8,35 @@ variable "region" {
 
 variable "cluster_name" {
   description = "Name of the GKE cluster"
+  default     = "my-gke-cluster"
 }
 
 variable "vpc_name" {
   description = "Name of the VPC"
+  default     = "my-vpc"
 }
 
 variable "subnet_name" {
   description = "Name of the subnet"
+  default     = "my-subnet"
 }
 
 variable "node_pool_name" {
   description = "Name of the node pool"
+  default     = "default-pool"
 }
 
 variable "machine_type" {
-  description = "Machine type for GKE  nodes"
+  description = "Machine type for GKE nodes"
+  default     = "e2-medium"
+}
+
+variable "ip_range_pods" {
+  description = "IP range for GKE pods"
+  default     = "192.168.0.0/16"
+}
+
+variable "ip_range_services" {
+  description = "IP range for GKE services"
+  default     = "10.0.0.0/20"
 }
