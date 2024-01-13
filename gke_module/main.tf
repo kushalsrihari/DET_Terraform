3e7fb7836275fce3c70e1a16acabd111d2e38394
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 10:19:19 +0530

    Update main.tf

--- gke_module/main.tf
+++ gke_module/main.tf
@@ -1,27 +1,14 @@
-provider "google" {
-  credentials = file("../credentials/awesome-gist-402911-68434aac7771.json")
-  project     = var.project_id
-  region      = var.region
-}
+resource "google_container_cluster" "cluster" {
+  name     = var.cluster_name
+  location = var.region
 
-module "gke" {
-  source          = "terraform-google-modules/kubernetes-engine/google"
+  network    = var.vpc_name
+  subnetwork = var.subnet_id
 
-  cluster_name     = var.gke_cluster_name
-  project_id       = var.project_id
-  region           = var.region
-  network          = var.vpc_name
-  subnetwork       = var.subnet_name
-  subnetwork_project = var.project_id
-  node_pool_name   = var.node_pool_name
-  machine_type     = var.machine_type
-  node_count       = 1
-  node_locations   = [var.region]
-  enable_autoscaling = true
-  enable_network_policy = false
-  create_service_account = false
-  ip_range_pods    = var.ip_range_pods
-  ip_range_services = var.ip_range_services
-  subnetwork_ip_ranges   = [var.ip_range_pods]
-  subnetwork_secondary_ip_ranges = {}
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = var.ip_range_pods
+    services_ipv4_cidr_block = var.ip_range_services
+  }
+
+  # Configure node pools, IP masquerading, etc. as needed
 }
