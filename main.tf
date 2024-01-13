087dc151c2849391b01d60730007f51c2cdbfb51
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 09:37:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,15 +6,8 @@ provider "google" {
 
 module "vpc" {
   source    = "./vpc_module"
-  project_id = var.project_id
-  region     = var.region
 }
 
 module "gke" {
   source          = "./gke_module"
-  project_id      = var.project_id
-  region          = var.region
-  cluster_name    = var.cluster_name
-  node_pool_name  = var.node_pool_name
-  machine_type    = var.machine_type
 }
