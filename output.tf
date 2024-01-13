2565ad626c4548fd13d2c16ab7537947fd52e273
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 10:17:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,18 +2,14 @@ output "gke_cluster_name" {
   value = module.gke.cluster_name
 }
 
-output "gke_cluster_endpoint" {
-  value = module.gke.cluster_endpoint
+output "gke_endpoint" {
+  value = module.gke.endpoint
 }
 
-output "gke_cluster_security_group_ids" {
-  value = module.gke.cluster_security_group_ids
+output "vpc_name" {
+  value = module.vpc.vpc_name
 }
 
-output "vpc_network_name" {
-  value = module.vpc.network_name
-}
-
-output "vpc_subnet_name" {
-  value = module.vpc.subnetwork_name
+output "subnet_name" {
+  value = module.vpc.subnet_name
 }
