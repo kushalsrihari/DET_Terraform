7bbe2593c3d569ad923247fc700d8cc5c9a254a2
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2024-01-13 10:17:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.49.0"
+    }
+  }
+}
+
 provider "google" {
-  credentials = file("./credentials/awesome-gist-402911-68434aac7771.json")
-  project     = var.project_id
-  region      = var.region
+  credentials = file("../credentials/awesome-gist-402911-68434aac7771.json")
+  project = var.project_id
+  region  = var.region
 }
 
 module "vpc" {
-  source    = "./vpc_module"
+  source = "./modules/vpc"
+
+  project_id   = var.project_id
+  region       = var.region
+  vpc_name     = "my-vpc"
+  subnet_name  = "my-subnet"
+  cidr_block   = "10.0.0.0/16"
 }
 
 module "gke" {
-  source          = "./gke_module"
-  project_id      = var.project_id
-  region          = var.region
-  gke_cluster_name = var.gke_cluster_name
-  vpc_name         = module.vpc.network_name
-  subnet_name      = module.vpc.subnetwork_name
-  node_pool_name   = var.node_pool_name
-  machine_type     = var.machine_type
+  source = "./modules/gke"
+
+  project_id       = var.project_id
+  region           = var.region
+  cluster_name     = "my-gke-cluster"
+  subnet_id        = module.vpc.subnet_id
+  ip_range_pods    = "10.2.0.0/16"
+  ip_range_services = "10.3.0.0/24"
 }
